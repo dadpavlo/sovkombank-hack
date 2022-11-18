15b4ffa18c440fc9f9306e5837b9547b3b7ba70e
Veronika Rodina <veronika.rodina@nordclan.com>
2022-11-19 01:24:00 +0300

    Поднятие рестов

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from flask import Flask
 from flask import request
 import db_api
@@ -12,12 +14,43 @@ def index():
     return 'Hello World'
 
 
-@app.route('/signUp')
+@app.route('/signUp', methods=['POST'])
+def signup_post():
+    login = request.args.get('login')
+    password = request.args.get('password')
+    user = db_api.get_user_by_login(login)
+    if user is None:
+        db_api.create_user(login, password, 0)
+    else
+
+
+    return 'created user'
+
+@app.route('/login')
 def sign_up():
     login = request.args.get('login')
     password = request.args.get('password')
-    db_api.create_user(login, password, 0)
+    user = db_api.get_user_by_login(login)
+    if user is not None:
+        if user.password == password:
+            rand_token = uuid4()
+
     return 'created user'
+
+## TODO удалять может только админ
+@app.route('/deleteUser')
+def delete_user():
+    user_id = request.args.get('userId')
+    db_api.delete_user(user_id)
+    return 'user deleted'
+
+
+@app.route('/getUserById')
+def get_user_by_id():
+    user_id = request.args.get('userId')
+    user = db_api.get_user_by_id(user_id)
+    return str(user.user_id)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
