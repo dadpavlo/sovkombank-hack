6822be079b50a23f1ec46742985b39bdd8b2de06
olegmarkelov <vvlrff@mail.ru>
2022-11-19 02:07:41 +0300

    [FRONT] реализованы основы пользователя

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 import Users from './Users';
 
 const Admin = () => {
-    return (
-        <section>
-            <h1>Admins Page</h1>
-            <br />
-            <Users />
-            <br />
-            <div className="flexGrow">
-                <Link to="/">Home</Link>
-            </div>
-        </section>
-    )
+  return (
+    <section>
+      <h1>Страница администратора</h1>
+      <br />
+      <Users />
+      <br />
+      <div className="flexGrow">
+        <Link to="/">На главную</Link>
+      </div>
+    </section>
+  )
 }
 
 export default Admin
