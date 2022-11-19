f2fa5275e672b41086a7d15e52a19b5810658adc
olegmarkelov <vvlrff@mail.ru>
2022-11-19 13:23:36 +0300

    ююю

--- backend/db_create.py
+++ backend/db_create.py
@@ -1,37 +1,69 @@
 import sqlite3
 
-def create_roles(db_name : str):
-    with sqlite3.connect(db_name) as db:
-        cursor = db.cursor()
-        query = """
-                    CREATE TABLE IF NOT EXISTS roles(
-                    role_id	INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                    name_role	TEXT
-                    );"""
-        print(query)
-        cursor.execute(query)
-        db.commit()
+# def create_roles(db_name : str):
+#     with sqlite3.connect(db_name) as db:
+#         cursor = db.cursor()
+#         query = """
+#                     CREATE TABLE IF NOT EXISTS roles(
+#                     role_id	INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+#                     name_role	TEXT
+#                     );"""
+#         print(query)
+#         cursor.execute(query)
+#         db.commit()
+#
+# def create_users(db_name : str):
+#     with sqlite3.connect(db_name) as db:
+#         cursor = db.cursor()
+#         query = """
+#                     CREATE TABLE IF NOT EXISTS users(
+#                     user_id	INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+#                     login	TEXT ,
+#                     password	BLOB,
+#                     salt BLOB,
+#                     role_id	INTEGER,
+#                     FOREIGN KEY(role_id) REFERENCES roles(role_id)
+#                     );"""
+#         print(query)
+#         cursor.execute(query)
+#         db.commit()
 
-def create_users(db_name : str):
+def create_accounts(db_name:str):
     with sqlite3.connect(db_name) as db:
         cursor = db.cursor()
         query = """
-                    CREATE TABLE IF NOT EXISTS users(
-                    user_id	INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                    login	TEXT ,
-                    password	BLOB,
-                    salt BLOB,
-                    role_id	INTEGER,
-                    FOREIGN KEY(role_id) REFERENCES roles(role_id)
-                    );"""
+                CREATE TABLE IF NOT EXISTS accounts (
+                	"account_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                	"type"	INTEGER,
+                	"remains"	REAL,
+                	"status"	INTEGER,
+                	"user_id"	INTEGER,
+                	FOREIGN KEY("user_id") REFERENCES "users"("user_id")
+                );"""
         print(query)
         cursor.execute(query)
-        db.commit()
+        db.commit()   
+
+def create_operations(db_name: str):
+        with sqlite3.connect(db_name) as db:
+            cursor = db.cursor()
+            query = """
+            CREATE TABLE IF NOT EXISTS "operations" (
+                        "operation_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        "amount"	INTEGER,
+                        "account_id"	INTEGER,
+                        "time"	INTEGER,
+                        "type"	INTEGER,
+                        FOREIGN KEY("account_id") REFERENCES "accounts"("account_id")
+                    );"""
+            print(query)
+            cursor.execute(query)
+            db.commit() 
 
 db_name = 'sovkombank_db'
-create_roles(db_name)
-create_users(db_name)
 
+create_accounts(db_name)
+create_operations(db_name)
 print('end')
 
 
